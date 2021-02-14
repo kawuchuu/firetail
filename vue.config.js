9f7ac0b445ea043a82c6ffbc51542bfc26b154bd
kawuchuu <kawuchuu@users.noreply.github.com>
2021-02-15 00:50:57 +1100

    set publish to null

--- vue.config.js
+++ vue.config.js
@@ -58,7 +58,8 @@ module.exports = {
                         Icon: "firetail",
                         Terminal: "false"
                     }
-                }
+                },
+                publish: null
             }
         }
     }
