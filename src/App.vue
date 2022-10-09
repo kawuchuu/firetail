28faec1012841029a8a3609108585251416d6c93
kawuchuu <kawuchuu@users.noreply.github.com>
2022-10-09 14:34:45 +1100

    firetail logo no longer appears on side bar

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,6 @@
         <div class="drag-detail">
             <p id="dragInfo">Nothing selected</p>
         </div>
-        <div class="cover" style="transition:.25s;transition-property:opacity;width: 100%;height: 100%;position: fixed;z-index: 11;background: linear-gradient(#e74e8e, #ef9135);display: none;justify-content: center;align-items: center;">
-            <!-- generated svg, slightly modified -->
-            <svg style="width:150px;height:150px;" viewBox="0 0 1080 1080">
-                <path class="st0" d="M653.4,462.8c-57.9-50-115.1-163.8-138.3-268.2c-42.6,37.6-109.4,138.7-97.3,281.5"/>
-                <g>
-                    <path class="st1" d="M299.9,861.7c-28.6-18.9-51.1-40.6-51.1-40.6c-19.1-18.4-33-36-42.5-49.5l0,0
-                        c133.7,18.1,198.3-24.1,221.3-42.4c18.6-14.7,39.2-37,59-58.4c41.9-45.2,66.7-80.1,85.7-105.6C659.3,448.8,705,400.8,745.2,386.6
-                        c68.6-24.3,117,32,160.2,53.6c0,0,0.2,0.1,0.2,0.1c-0.2,0.1-0.5,0.4-0.8,0.7c-39.6,35.1-56.8,104.8-63.9,179.3
-                        C830.4,731.4,764,824.2,670,884.3c-58.4,37.3-123.4,58.6-184.3,52.4C401.4,928,306.3,865.9,299.9,861.7z"/>
-                </g>
-                <path class="st1" d="M152.8,211.5"/>
-                <path class="st1" d="M450.4,710.8c-332.9,7.1-254.1-463.4-254.1-463.4c39.2,59.5,53.8,76.5,83.5,112.9c56.5,69,95.3,84.4,151.3,119.1c11.6,7.2,28.8,17.1,53.3,34.7c35.1,25.1,61.5,49,78.7,65.6"/>
-            </svg>
-        </div>
         <div :class="showDragIndicator" @dragleave="changeDrag(false)" @drop="filesDropped" class="drag-indicator">
             <div class="drag-msg">
                 <h1>Drop your music here!</h1>
