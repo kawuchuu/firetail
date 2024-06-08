875ae2d79a2f00854898321adeaff043cba32c03
kawuchuu <kawuchuu@users.noreply.github.com>
2024-06-08 11:32:40 +1000

    started some stuff but need to move to my laptop

---
+++ src/components/IndSongList.vue
@@ -0,0 +1,43 @@
+<script setup>
+import SongItem from '../router/components/SongItem.vue'
+import {ref} from "vue";
+import { useRouter, useRoute } from 'vue-router/composables'
+
+const router = useRouter()
+
+const props = defineProps({
+  list: Array
+})
+
+const selectedItems = ref([])
+const lastSelectedIndex = ref(0)
+
+const itemSizeCheck = () => {
+  if (.path === '/') {
+    return 42
+  } else {
+    return 55
+  }
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <virtual-list
+        :data-key="'id'"
+        :data-sources="list"
+        :data-component="SongItem"
+        :page-mode="true"
+        :page-mode-el="'#main-container'"
+        :extra-props="{selectedItems: selectedItems}"
+        :estimate-size="itemSizeCheck"
+        :keeps="40"
+        ref="virtualList"
+        @selected="select"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
