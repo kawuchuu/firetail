15772996c976492d9e04201bd084df320a8787ee
kawuchuu <kawuchuu@users.noreply.github.com>
2024-11-22 15:26:15 +1100

    some changes from a couple weeks back

--- src/components/tour/SidebarTour.vue
+++ src/components/tour/SidebarTour.vue
@@ -11,23 +11,38 @@ export default {
       text: [
         {
           title: 'Sidebar',
-          desc: "This is the main sidebar. Use these buttons to navigate through most of the app."
+          desc: "This is the main sidebar. Use these buttons to navigate through most of the app.",
+          pointing: 'left'
         },
         {
           title: 'Songs',
-          desc: 'All songs in your library are displayed here in one list.'
+          desc: 'All songs in your library are displayed here in one list.',
+          pointing: 'left'
         },
         {
           title: 'Artists',
-          desc: 'Split up the long song list and filter by artist here.'
+          desc: 'Split up the long song list and filter by artist here.',
+          pointing: 'left'
         },
         {
           title: 'Albums',
-          desc: 'Want to listen to a single album? View them all here.'
+          desc: 'Want to listen to a single album? View them all here.',
+          pointing: 'left'
         },
         {
           title: 'Favourites',
-          desc: 'Have a few songs you really enjoy? All your favourites are here.'
+          desc: 'Have a few songs you really enjoy? All your favourites are here.',
+          pointing: 'left'
+        },
+        {
+          title: 'Playlists',
+          desc: 'Want to only listen to select songs? Create and view playlists here.',
+          pointing: 'left'
+        },
+        {
+          title: 'Playing Bar',
+          desc: "This is the playing bar. You can view and control everything that's currently playing here.",
+          pointing: 'down'
         }
       ]
     }
@@ -55,7 +70,7 @@ export default {
 <template>
 <div class="sidebar-tour">
   <div class="highlight-tour" :class="`sidebar-${part}`"></div>
-  <PointingArrow :class="`pa-${part}`" :pointing="'left'"/>
+  <PointingArrow :class="`pa-${part}`" :pointing="text[part].pointing" />
   <TourSheet :class="`ts-${part}`">
     <h1>{{this.text[this.part].title}}</h1>
     <p>{{this.text[this.part].desc}}</p>
@@ -141,5 +156,50 @@ export default {
   width: calc(var(--sidebar-width) - 20px);
   height: 47px;
   border-radius: 14px;
+}
+
+.pa-5 {
+  position: fixed;
+  top: 295px;
+  left: calc(var(--sidebar-width) + 20px);
+}
+.ts-5 {
+  top: 285px;
+  left: calc(var(--sidebar-width) + 80px);
+}
+.highlight-tour.sidebar-5 {
+  top: 260px;
+  width: calc(var(--sidebar-width) - 20px);
+  height: calc(100vh - 85px - 280px);
+}
+
+.pa-5 {
+  position: fixed;
+  top: 295px;
+  left: calc(var(--sidebar-width) + 20px);
+}
+.ts-5 {
+  top: 285px;
+  left: calc(var(--sidebar-width) + 80px);
+}
+.highlight-tour.sidebar-5 {
+  top: 260px;
+  width: calc(var(--sidebar-width) - 20px);
+  height: calc(100vh - 85px - 280px);
+}
+
+.pa-6 {
+  position: fixed;
+  top: calc(100vh - 125px);
+  left: calc(50vw - 20px);
+}
+.ts-6 {
+  top: calc(100vh - 350px);
+  left: calc(50vw - 125px);
+}
+.highlight-tour.sidebar-6 {
+  top: calc(100vh - 85px);
+  width: calc(100vw - 20px);
+  height: 65px;
 }
 </style>
